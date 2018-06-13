277f5586effde7edbf7d1943fc6d5e581b5bbfcf
zhesha <savzhe@gmail.com>
2018-06-13 17:57:35 +0300

    menu scene

---
+++ Unity/Beryllium/Assets/Scripts/Behaviors/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public Button OnePlayerButton;
+    public Button TwoPlayerButton;
+
+    public Button AntiAircraftButton;
+    public Button TorpedoButton;
+    public Button ShootingGallaryButton;
+    public Button BomberButton;
+
+    public Button PlayButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
